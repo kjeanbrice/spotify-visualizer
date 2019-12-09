9139d608ced55e2880f505a514c27a567af95851
Karl Jean-Brice <kjeanbri@hotmail.com>
2019-12-09 04:06:13 -0500

    Added a responsive side menu nagivation for upcoming pages

--- frontend/src/app/profile/profile.module.ts
+++ frontend/src/app/profile/profile.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProfileComponent} from './profilecomponent/profile.component';
 import {ProfileRoutingModule} from './profile-routing.module';
+import {MenuModule} from '../menu/menu.module';
 
 @NgModule({
     declarations: [
-        ProfileComponent
+        ProfileComponent,
     ],
     imports: [
         CommonModule,
-        ProfileRoutingModule
+        ProfileRoutingModule,
+        MenuModule
     ],
     providers: [
     ],
--- frontend/src/app/profile/profilecomponent/profile.component.ts
+++ frontend/src/app/profile/profilecomponent/profile.component.ts
@@ -1,11 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Feather from 'feather-icons';
+declare var jQuery: any;
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {}
+export class ProfileComponent implements OnInit{
+    constructor() { }
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        Feather.replace();
+    }
 }
